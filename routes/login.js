d33ed630334db7aede8dede05d44b328326f523f
vivek shekhawat <vshekhawat136@gmail.com>
2026-01-03 22:41:41 +0530

    changed

--- routes/login.js
+++ routes/login.js
@@ -4,15 +4,14 @@ export const Login=async(req,res)=>{
      const {name,email,passwd:password}=req.body
      const finded=await modelLogin.find({name,email,password})
      if(finded){
-          res.cookie('sessionId',dataId.id,{
+     res.cookie('sessionId',finded.id,{
       httpOnly:'true',
   sameSite: 'none',
   secure: true, 
   maxAge: 1000 * 60 * 60 * 24,
-     })
-     return res.status(201).json({mess:'next',data:`${finded.id}`})
+     })   
+     return res.status(201).json({mess:'next',data:`${finded.id}`}) 
      }
-     else{
      const dataId=await modelLogin.create({name,email,password})
      res.cookie('sessionId',dataId.id,{
       httpOnly:'true',
@@ -20,6 +19,6 @@ export const Login=async(req,res)=>{
   secure: true, 
   maxAge: 1000 * 60 * 60 * 24,
      })
-     return res.status(201).json({mess:'next',data:`${dataId.id}`})
-     }
+     res.status(201).json({mess:'next',data:`${dataId.id}`})
+     
 }
