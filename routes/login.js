0b49735395c75461a5ac5bf0f04d8d0b565de0eb
vivek shekhawat <vshekhawat136@gmail.com>
2026-01-03 23:04:47 +0530

    operate

--- routes/login.js
+++ routes/login.js
@@ -3,14 +3,14 @@ import { modelLogin } from '../setup/mongooseValidation.js'
 export const Login=async(req,res)=>{
      const {name,email,passwd:password}=req.body
      const finded=await modelLogin.find({name,email,password})
-     if(finded.id){
+     if(finded){
      res.cookie('sessionId',finded.id,{
       httpOnly:'true',
   sameSite: 'none',
   secure: true, 
   maxAge: 1000 * 60 * 60 * 24,
      })   
-     return res.status(201).json({mess:'n',data:`${finded.id}`}) 
+     return res.status(201).json({mess:'next',data:`${finded.id}`}) 
      }
      const dataId=await modelLogin.create({name,email,password})
      res.cookie('sessionId',dataId.id,{
